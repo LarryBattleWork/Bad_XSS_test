d757557a35938801caa5fcd4b8ea1c8135a4d232
Larry Battle at Work <larry.battle.work@gmail.com>
2016-10-27 16:23:46 -0500

    Update xss_test.js

--- xss_test.js
+++ xss_test.js
@@ -1,1 +1,3 @@
-window.location = "https://github.com/LarryBattleWork/Bad_XSS_test";
+if(window.confirm("YOU WON a FREE TRIP! Click ok to accept")){
+  window.location = "https://github.com/LarryBattleWork/Bad_XSS_test";
+}
